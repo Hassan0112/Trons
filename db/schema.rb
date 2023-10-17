1bbe8abf9fd3e8bbad3375eccb40b6cd6434a151
Hassan0112 <Bhattiali4469@gmail.com>
2023-10-17 15:47:12 +0000

    TRON-6 Add Fields and full name to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_211432) do
+ActiveRecord::Schema.define(version: 2023_10_16_233836) do
 
   create_table "users", charset: "latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 2023_10_10_211432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "full_name"
     t.string "gender"
     t.date "dob"
     t.string "country"
     t.string "city"
     t.string "username"
+    t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
