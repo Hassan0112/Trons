35852d37a8486324d47be7c5f9e20f7617d36937
Hassan0112 <Bhattiali4469@gmail.com>
2023-10-17 17:18:37 +0000

    TRON-6 Add Fields to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_16_233836) do
+ActiveRecord::Schema.define(version: 2023_10_10_211432) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2023_10_16_233836) do
     t.string "country"
     t.string "city"
     t.string "username"
-    t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
