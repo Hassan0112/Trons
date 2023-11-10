95d42baf657446600a1d09bc6ae7e47465df9c9c
Hassan0112 <Bhattiali4469@gmail.com>
2023-11-10 16:38:45 +0000

    TRON-8

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
   has_one_attached :profile_picture
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  
 end
