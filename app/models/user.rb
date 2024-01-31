d97389fcebb0bfcd7c97f5ee61df04f545a8625a
Muhammad Ahmad Shahbaz <muhammadahmadshahbaz786@gmail.com>
2024-01-31 23:15:44 +0500

    TRON-18: add migration and create model for UserAdd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ApplicationRecord  
   devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
   has_one_attached :profile_picture
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :price, presence: true, 
             format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
-
-  validates :password, :password_confirmation, presence: true
-  
+  validates :password, :password_confirmation, presence: true  
+  has_many: user_adds
 end
