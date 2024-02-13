054079c570634ca687d0c7070e79e347924d02d3
Ali Hassan <bhattiali4469@gmail.com>
2024-02-13 23:47:46 +0500

    TRON-16: discard route changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   patch '/profile', to: 'users#update'
   get '/profile', to: 'users#profile', as: 'user_profile'
   patch '/update_profile_picture', to: 'users#update_profile_picture', as: 'update_profile_picture'
-  get 'used-cars/sell', to: 'used_cars#new_ad'
+  get 'used-cars/sell', to: 'used_cars#post_ad'
   get '/post-ad' , to: 'used_cars#new_ad'
   get '/change_password', to: 'users#edit_password', as: 'change_password'
   patch '/change_password', to: 'users#update_password'
