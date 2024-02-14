ec1194b1f4067b129e1adebdfb3b95df752996f2
Ali Hassan <bhattiali4469@gmail.com>
2024-02-14 07:57:22 +0500

    TRON-13-revamp-fix: Exterior color fix on new add form

--- app/javascript/packs/new_ad.js
+++ app/javascript/packs/new_ad.js
@@ -223,3 +223,18 @@ $(document).ready(function() {
 
   })
 });
+
+$(document).ready(function() {
+  const colorSelect = $('#car_color');
+  const colorPreview = $('#color-preview');
+
+  function updateColorPreview() {
+    const selectedColor = colorSelect.find(':selected');
+    const backgroundColor = selectedColor.data('color') || 'transparent';
+    colorPreview.css('background-color', backgroundColor);
+  }
+
+  colorSelect.change(updateColorPreview);
+
+  updateColorPreview();
+});
